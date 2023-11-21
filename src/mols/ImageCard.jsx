a6a7a39a1c092c2f57944bb7ec0b97838ff705b4
sakaki <oisiitakuwan@gmail.com>
2023-11-21 23:13:48 +0900

    🐛リンクに飛べない問題を修正

--- src/mols/ImageCard.jsx
+++ src/mols/ImageCard.jsx
@@ -72,13 +72,13 @@ export const ImageCard = ({imageInfo}) => {
                 icon={<TwitterOutlined />}
                 type="primary"
                 style={{ right: "80px" }}
-                href={imageInfo.twitterUrl}
+                onClick={() => window.open(imageInfo.twitterUrl, "_blank")}
             />
             <LinkFloatButton
                 description="Pixiv"
                 type="primary"
                 style={{ right: "20px" }}
-                href={imageInfo.pixivUrl}
+                onClick={() => window.open(imageInfo.pixivUrl, "_blank")}
             />
         </ImageCardItem>
     );
